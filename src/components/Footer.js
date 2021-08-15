c1e9b826a94d1b1687477b7e2fa1a8907d326f5b
ZiyuWang-114 <ziyuwang1104@gmail.com>
2021-08-15 11:32:52 -0500

    adding files for the github pages

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2020 StarLink Tracker. All Rights Reserved. Website Made by Ziyu Wang
+            </footer>
+        )
+    }
+}
+export default Footer;
